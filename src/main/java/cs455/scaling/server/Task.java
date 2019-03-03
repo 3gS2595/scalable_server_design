0f1cb31035215de1e4485f0701022329cf3c1442
DunesWake <dbtrinkner@gmail.com>
2019-03-02 19:30:48 -0700

    task just a start

--- src/main/java/cs455/scaling/server/Task.java
+++ src/main/java/cs455/scaling/server/Task.java
@@ -2,9 +2,14 @@ package cs455.scaling.server;
 
 import java.nio.channels.SocketChannel;
 
-public class Task {
+public class Task implements Runnable {
     private SocketChannel client;
     public Task(SocketChannel client){
         this.client = client;
     }
+
+    public void run() {
+        System.out.println("Task " + " is running.");
+    }
 }
+
